f7642df1167142e0fd650c64d33e6037c11cd2a6
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2024-03-21 16:36:33 +0000

    Variable declaration is done.

---
+++ ch.1/01_Variables.js
@@ -0,0 +1,18 @@
+const accountId = 123456 
+let accountEmail = "MashoodAli@gmail.com"
+var accountPassword = "1234"
+accountCity = "RahimYar Khan"
+let province; 
+
+// console.log(accountID);//Const is not allowed to change
+console.log(accountId);
+accountEmail= "Ali@gmail.com";
+accountPassword = "123456789";
+accountCity = "Lahore";
+
+console.table([accountId, accountEmail, accountEmail,accountCity ]);
+
+// Summary 
+// CONST: The const variables are not allowd to change the value and Const is block scoped. OR you cannot change the CONST variable value in future in the same program.
+// LET: The LET is also same like block scoped but you can change the value in future. 
+//VAR: The VAR declaration is globally scoped or function scoped. 
