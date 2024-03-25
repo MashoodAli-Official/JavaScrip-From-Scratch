b42ffac0d663c91f2274105e4e0a4bfc82f6004f
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2024-03-25 11:52:23 +0000

    Completed chapter: Comparision

---
+++ ch.1/04_Comparision.js
@@ -0,0 +1,37 @@
+// 🚨 COMPARISION OPERATORS 
+// COMPARISION OPERATORS ARE USED TO TEST FOR "TRUE" OR "FALSE"
+
+let a = 10;
+let b = 5;
+// 👇 There are different comparision operators , like 
+// equal to ==
+console.log( a == b); // ➡️ output false
+console.log( b == a); // ➡️ output false
+
+// equal to and equal type to === 
+console.log(a === b); //➡️ output false
+console.log("a" === b); //➡️ output false
+
+// not equal != 
+console.log( a != b); //➡️ output true
+console.log(b != a); //➡️ output true
+
+// not equal and not equal type !==
+console.log(a !== b); //➡️ output true
+console.log("a" !== b); //➡️ output true
+
+// greater than >
+console.log(a > b);//➡️ output true
+console.log(b > a);//➡️ output false
+
+// less than <
+console.log(a < b); //➡️ output false
+console.log(b < a); //➡️ output true
+
+// greater than and equal >=
+console.log(a >= b); //➡️ output true
+console.log(b >= a); //➡️ output false
+
+// less than and equal <=
+console.log(a <= b); //➡️ output false
+console.log(b <= a); //➡️ output true
