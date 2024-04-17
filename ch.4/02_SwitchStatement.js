3b4100035ba47e20cb58dd0b6c3cd73104bb7da9
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2024-04-17 18:27:48 +0000

    chapter completed: control flow

---
+++ ch.4/02_SwitchStatement.js
@@ -0,0 +1,19 @@
+// 👇👇 Switch Statement in Javascript
+
+//The switch case statement in JavaScript is also used for decision-making purposes. 
+// In some cases, using the switch case statement is seen to be more convenient than if-else statements.
+var num = 2;
+switch (num) {
+    case 0 : 
+        console.log('Number is equal to zero ');
+        break;
+    case 2 :
+        console.log("Number is equal to 2");
+        break;
+    case 3 :
+        console.log("Number is equal to 3");
+        break;
+    default:
+        console.log("Number is equal to 10 or greater than 10"); 
+};
+// output => Number is equal to 2
---
+++ ch.4/03_TernaryOperator.js
@@ -0,0 +1,14 @@
+//👇👇 Ternary operator in javascript
+
+// The conditional operator, also referred to as the ternary operator (?:), 
+// is a shortcut for expressing conditional statements in JavaScript.
+
+//SYTEX OF TERNARY (?) OPERATOR 
+//condition ? value if true : value if false
+
+var num = 5;
+var result = num >= 8 ? 'Greater than 8' : 'Smaller then 8 ';
+console.log(`The number ${num} is ${result}`);
+
+//Using backticks and ${} makes it straightforward to insert variables directly into a string. 
+//This makes it simpler to include dynamic values in strings without having to concatenate parts using the plus (+) operator.
