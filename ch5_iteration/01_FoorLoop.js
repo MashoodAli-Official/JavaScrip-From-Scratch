fc44cfeb1b87b776cee87d29e3977eaa6c4c5f20
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2024-04-19 23:47:50 +0500

    Foor Loop

---
+++ ch5_iteration/01_FoorLoop.js
@@ -0,0 +1,16 @@
+//FOR LOOP IN JAVASCRIPT
+// The for loop in JavaScript is a fundamental construct used to execute a block of code repeatedly.
+// initializationExpression: This expression is executed only once at the beginning of the loop. It's commonly used to declare and initialize a loop counter variable.
+// conditionExpression: This expression is evaluated before each iteration of the loop. If the expression evaluates to true, the loop body code is executed. If it's false, the loop terminates.
+// updateExpression: This expression is executed after each iteration of the loop body. It's typically used to update the loop counter variable.
+
+for (let i=0; i<5; 1++){
+  console.log("This Foor loop", i );
+}
+
+// In this example:
+
+// let i = 0 (initialization): Initializes a variable i with the value 0.
+// i < 5 (condition): Checks if i is less than 5.
+// i++ (update): Increments the value of i by 1 after each iteration.
+// This loop will print "Iteration" followed by the current value of i (0, 1, 2, 3, 4) to the console five times.
